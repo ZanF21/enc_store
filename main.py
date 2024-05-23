9f0fadad4adf4500d7f1cd6f7f7cc31ec6de8775
rizan21 <rizanfarooqui@gmail.com>
2024-05-23 12:26:30 +0530

    Added delete - More colours! - `list_all` feature to see all password (in `Advanced Options`) - Using `tree` instead (cause it looks cool) - `select_path` for .... idk ease of use ig - more exception handeling
    Signed-off-by: rizan21 <rizanfarooqui@gmail.com>

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ def run(cipher):
     if not isInitialized():
         init()
     print("1. View\n2. Add\n3. Delete\n4. Exit")
-    choice = input()
+    choice = input(": ")
     os.system("clear")
 
     try:
@@ -40,7 +40,7 @@ def run(cipher):
     elif choice_int == 2:
         add(cipher)
     elif choice_int == 3:
-        delete()
+        delete(cipher)
     elif choice_int == 4:
         return False
     elif choice_int == 0:
@@ -52,22 +52,35 @@ def run(cipher):
 
 def advanced():
     print("Advanced Options")
-    print("1. Initialize")
-    print("2. Export all passwords")
-    choice = input()
-
+    print("1. Initialize\n2. Export\n3. List All\n0. Go back")
+    choice = input(": ")
+    os.system("clear")
     try:
         choice_int = int(choice)
     except ValueError:
-        print("Invalid Input (not a number)")
+        print(bcolors.FAIL + "Invalid Input (not a number)" + bcolors.ENDC)
         return
 
     if choice_int == 1:
         init()
     elif choice_int == 2:
         export()
+    elif choice_int == 3:
+        list_all()
+    elif choice_int == 0:
+        return
     else:
-        print("Invalid choice")
+        print(bcolors.FAIL + "Invalid choice, going back" + bcolors.ENDC)
+    return
+
+
+def list_all():
+    pwd = os.getcwd()
+    os.chdir(INIT_PATH)
+    print("All Stored Passwords:")
+    os.system("tree . --noreport")
+    os.chdir(pwd)
+    print("\n")
     return
 
 
@@ -75,7 +88,6 @@ def export():
     os.system("cp -r " + INIT_PATH + " " + os.path.join("pass_export"))
     os.system(f"tar cfz pass_export.tar.gz pass_export ")
     os.system(f"rm -r pass_export")
-    os.system("clear")
     print(f"Exported to {os.path.join(CURR_DIR, 'pass_export.tar.gz')}")
 
 
@@ -98,6 +110,7 @@ def cipher_gen(prompt="Password: "):
 def main():
     again = True
     cipher = cipher_gen()
+    os.system("clear")
     while again:
         again = run(cipher)
 
@@ -107,85 +120,38 @@ def isInitialized():
 
 
 def view(cipher):
-    path_followed = INIT_PATH
-    if not os.listdir(path_followed):
-        print("Nothing Stored")
-        return
-    go_deeper = True
-    while go_deeper:
-        l = [
-            x
-            for x in os.listdir(path_followed)
-            if os.path.isdir(os.path.join(path_followed, x))
-        ]
-        l.sort()
-        if not l:
-            break
-        for i, item in enumerate(l):
-            print(str(i + 1) + ". " + item)
-        choice = input("\n ")
-        os.system("clear")
-        try:
-            choice_int = int(choice)
-        except ValueError:
-            print("Invalid Input (not a number)")
-            continue
-        if choice_int > len(l):
-            print("Invalid Choice")
-            continue
-        if choice_int == 0:
-            return
-        path_followed = os.path.join(path_followed, l[choice_int - 1])
-
-    for item in os.listdir(path_followed):
-        all_files = os.listdir(path_followed)
-        for i, file in enumerate(all_files):
-            print(str(i + 1) + ". " + file)
-        choice = input("\n ")
-        os.system("clear")
-        try:
-            choice_int = int(choice)
-        except ValueError:
-            print("Invalid Input (not a number)")
-            continue
-        if choice_int > len(all_files):
-            print("Invalid Choice")
-            continue
-        with open(os.path.join(path_followed, all_files[choice_int - 1]), "rb") as f:
+    try:
+        path = select_file()
+        with open(path, "rb") as f:
             try:
                 pyperclip.copy(cipher.decrypt(f.read()).decode())
-                print(bcolors.OKGREEN + "Copied to clipboard" + bcolors.ENDC)
+                short_path = path[len(INIT_PATH) + 1 :]
+                print(
+                    bcolors.OKGREEN + f"Copied {short_path} to clipboard" + bcolors.ENDC
+                )
                 exit(0)
             except Exception as e:
                 print(bcolors.FAIL + "incorrect key ig" + bcolors.ENDC)
-                exit(1)
-    return
+                exit(0)
+
+    except Exception as e:
+        print(e)
+        return
 
 
 def init():
     if isInitialized():
-        os.system("clear")
         print("Already initialized")
         return
     else:
         os.makedirs(INIT_PATH)
-        os.system("clear")
-        print("Initialized at " + INIT_PATH)
+        print(bcolors.OKGREEN + "Initialized at " + INIT_PATH + bcolors.ENDC + "\n")
 
 
 def add(cipher):
-#     find .pass_store -type d -print0 | while IFS= read -r -d '' dir; do
-#   if [ -z "$(find "$dir" -mindepth 1 -type d 2>/dev/null)" ]; then
-#     echo "$dir"
-#   fi
-# done
     print("Existing DIRs:")
     PWD = os.getcwd()
-    os.system("cd " + INIT_PATH)
-
-    cmd = "find . -type d -print0 | while IFS= read -r -d '' dir; do if [ -z \"$(find \"$dir\" -mindepth 1 -type d 2>/dev/null)\" ]; then echo \"$dir\"; fi; done"
-    os.system(cmd)
-    os.system("cd " + PWD)
+    os.system(f"cd {INIT_PATH} && tree . --noreport -d && cd {PWD}")
     print("\n")
     enc_path = input("Which DIR: ")
     file_name = input("Username: ")
@@ -201,5 +167,92 @@ def add(cipher):
     print("Added")
 
 
+def select_file():
+    path_followed = INIT_PATH
+
+    if not os.listdir(path_followed):
+        raise Exception(bcolors.OKBLUE + "Nothing Stored" + bcolors.ENDC)
+
+    go_deeper = True
+    while go_deeper:
+        l = [
+            x
+            for x in os.listdir(path_followed)
+            if os.path.isdir(os.path.join(path_followed, x))
+        ]
+        l.sort()
+        if not l:
+            go_deeper = False
+            continue  # basically a break, wrote it this way for readability
+
+        for i, item in enumerate(l):
+            print(str(i + 1) + ". " + item)
+        choice = input("\n: ")
+        os.system("clear")
+        try:
+            choice_int = int(choice)
+        except ValueError:
+            print(bcolors.FAIL + "Invalid Input (not a number)" + bcolors.ENDC)
+            continue
+
+        if choice_int > len(l):
+            print(bcolors.FAIL + "Invalid Choice" + bcolors.ENDC)
+            continue
+        if choice_int == 0:
+            raise Exception(bcolors.OKCYAN + "User Exited" + bcolors.ENDC)
+
+        path_followed = os.path.join(path_followed, l[choice_int - 1])
+
+    while True:
+        all_files = os.listdir(path_followed)
+        for i, file in enumerate(all_files):
+            print(str(i + 1) + ". " + file)
+        choice = input("\n ")
+        os.system("clear")
+        try:
+            choice_int_file = int(choice)
+        except ValueError:
+            print(bcolors.FAIL + "Invalid Input (not a number)" + bcolors.ENDC)
+            continue
+
+        if choice_int_file > len(all_files):
+            print(bcolors.FAIL + "Invalid Choice" + bcolors.ENDC)
+            continue
+        if choice_int_file == 0:
+            raise Exception(bcolors.OKCYAN + "User Exited" + bcolors.ENDC)
+        break
+
+    return os.path.join(path_followed, all_files[choice_int_file - 1])
+
+
+def delete(cipher):
+    try:
+        path = select_file()
+        with open(path, "rb") as f:
+            try:
+                pyperclip.copy(cipher.decrypt(f.read()).decode())
+                os.remove(path)
+                short_path = path[len(INIT_PATH) + 1 :]
+                os.system("find " + INIT_PATH + " -type d -empty -delete")
+                print(
+                    bcolors.OKGREEN
+                    + f"Deleted {short_path} and copied to clipboard (just in case)"
+                    + bcolors.ENDC
+                )
+            except Exception as e:
+                print(bcolors.FAIL + "incorrect key ig" + bcolors.ENDC)
+                exit(0)
+    except Exception as e:
+        print(e)
+    return
+
+
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("\nCTRL+C, exiting")
+        exit(0)
+    except EOFError:
+        print("\nCTRL+D, exiting")
+        exit(0)
